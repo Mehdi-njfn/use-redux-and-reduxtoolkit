4d4cd530bbfd42bf0f06d3ddec3da6e8e9320ac5
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-02-26 17:24:08 +0330

    use provider in appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route , Routes, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import Contact from './contact';
+import Login from './login';
+import { store } from './Store';
 
 function App() {
   return (
     <div className="App">
-      
+    <Provider store={store}>
+      <Router>
+        <Link style={{marginInline:'20px'}} to='/login' >login</Link>
+        <Link to='/contact' >contact</Link>
+        <Routes>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='/login' element={<Login />}/>
+        </Routes>
+      </Router>
+    </Provider>
     </div>
   );
 }
