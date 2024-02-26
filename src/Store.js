0852b6951cba2c3082c09935285282a21280a37f
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-02-26 17:23:19 +0330

    first func definations from redux and redux toolkit

---
+++ src/Store.js
@@ -0,0 +1,21 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState : {
+    username : ""
+  },
+  reducers : {
+    login: (state, action)=>{
+      state.username = action.payload.username;
+    },
+    logout: (state)=>{
+      state.username = ""
+    }
+  }
+})
+
+export const {login, logout} = userSlice.actions
+export const store = configureStore({reducer: {
+  user : userSlice.reducer
+}})
